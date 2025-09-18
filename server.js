7049ff1d8e26a69d2196c469405f47ebee7ea123
connordavidfitzgerald <hello@connordavidfitzgerald.com>
2025-09-18 12:19:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,18 +15,18 @@ app.get("/", async (req, res) => {
     let browser
     try {
         // Launch headless browser using puppeteer-extra
-       cbrowser = await puppeteer.launch({
-    args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-infobars',
-        '--window-position=0,0',
-        '--ignore-certifcate-errors',
-        '--ignore-certifcate-errors-spki-list',
-        '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36'
-    ],
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-});
+       browser = await puppeteer.launch({
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-infobars',
+                '--window-position=0,0',
+                '--ignore-certifcate-errors',
+                '--ignore-certifcate-errors-spki-list',
+                '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36'
+            ],
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+        });
 
         const page = await browser.newPage()
 
